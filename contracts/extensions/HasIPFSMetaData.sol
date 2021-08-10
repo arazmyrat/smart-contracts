c9ca50dc005707f731a1b50640cda4fc170faa72
jalil <jalil@1001.digital>
2021-08-11 00:53:12 +0200

    Refactor IPFS metadata

--- contracts/extensions/HasIPFSMetaData.sol
+++ contracts/extensions/HasIPFSMetaData.sol
@@ -10,12 +10,23 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 abstract contract HasIPFSMetaData is ERC721 {
     using Strings for uint256;
 
+    string public cid;
+
+    constructor (string memory _cid) {
+        cid = _cid;
+    }
+
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
 
         return string(abi.encodePacked(
             _baseURI(), "/", tokenId.toString(), ".json"
         ));
+    }
+
+    // Configure the baseURI for the tokenURI method.
+    function _baseURI() internal view virtual override returns (string memory) {
+        return string(abi.encodePacked("ipfs://", cid));
     }
 
 }
