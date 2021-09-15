e509b0865fc5e7fe344db4cd8f9832a97df12e5d
jalil <jalil@1001.digital>
2021-09-15 17:28:22 +0200

    Deploy the PunkScape contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,16 +5,19 @@ require('dotenv').config()
 
 // Import tasks
 require('./tasks/deployOneDayPunk')
+require('./tasks/deployPunkScape')
 require('./tasks/setSaleStart')
 
 const CHECK_ADDRESS = `0x32336A625aacFA08fe9723d901FFf92A7E3465c1`
 const CRYPTOPUNKS_ADDRESS = `0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB`
+const ONEDAYPUNKS_ADDRESS = `0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91`
 const HARDHAT_NETWORK_CONFIG = {
   chainId: 1337,
   libraries: {
     CheckAddress: CHECK_ADDRESS,
   },
   CryptoPunksAddress: CRYPTOPUNKS_ADDRESS,
+  OneDayPunksAddress: ONEDAYPUNKS_ADDRESS,
   forking: {
     url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
     blockNumber: 13050000,
@@ -42,6 +45,7 @@ module.exports = {
         CheckAddress: CHECK_ADDRESS,
       },
       CryptoPunksAddress: CRYPTOPUNKS_ADDRESS,
+      OneDayPunksAddress: ONEDAYPUNKS_ADDRESS,
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
@@ -50,6 +54,8 @@ module.exports = {
         CheckAddress: `0x029374cA831F9F4B04a0D896B8d00CcE05f30D8f`,
       },
       CryptoPunksAddress: '0x52c5170414e72f2364869387bd9684245118deed',
+      // FIXME
+      OneDayPunksAddress: '0x0',
     },
     localhost: HARDHAT_NETWORK_CONFIG,
     hardhat: HARDHAT_NETWORK_CONFIG,
---
+++ tasks/deployPunkScape.js
@@ -0,0 +1,27 @@
+task('deployPunkScape', `Deploys the PunkScape.sol Contract`)
+  .setAction(async () => {
+    const [deployer] = await ethers.getSigners()
+    const networkConfig = hre.config.networks[hre.network.name]
+
+    console.log('Deploying contracts with the account:', deployer.address)
+    console.log('Account Balance:', (await deployer.getBalance()).toString())
+    console.log('MetaData CID:', process.env.SCAPES_METADATA_CID)
+    console.log('Sale Start:', process.env.START_SALE)
+    console.log('Contract Metadata URL:', process.env.SCAPES_CONTRACT_METADATA_URL)
+    console.log('CryptoPunk Contract Address:', networkConfig.CryptoPunksAddress)
+
+    const PunkScape = await ethers.getContractFactory('PunkScape', {
+      libraries: networkConfig.libraries,
+    })
+
+    const contract = await PunkScape.deploy(
+      deployer.address,
+      process.env.SCAPES_METADATA_CID,
+      process.env.START_SALE,
+      process.env.SCAPES_CONTRACT_METADATA_URL,
+      networkConfig.CryptoPunksAddress,
+      networkConfig.OneDayPunkAddress,
+    )
+
+    console.log('PunkScape Contract Address:', contract.address)
+  })
