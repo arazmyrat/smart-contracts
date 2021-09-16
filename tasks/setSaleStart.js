7cf22538941fbaded0ed1c9f63e74ce04c9b30ef
jalil <jalil@1001.digital>
2021-09-16 17:13:25 +0400

    setSaleStart fixes

--- tasks/setSaleStart.js
+++ tasks/setSaleStart.js
@@ -1,22 +1,21 @@
 const { nowInSeconds } = require('../helpers/time')
-require('hardhat/config')
 
-// const DEFAULT_CONTRACT = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
-// const abi = require('./../artifacts/contracts/PunkScape.sol/PunkScape.json').abi
+const DEFAULT_CONTRACT = process.env.PUNKSCAPE_CONTRACT_ADDRESS
+const abi = require('./../artifacts/contracts/PunkScape.sol/PunkScape.json').abi
 
-// task('setSaleStart', `Sets the start of the sale`)
-//   .addOptionalParam('address', `The contract address`)
-//   .addOptionalParam('time', `The timestamp in seconds since Unix Epoch`)
-//   .setAction(async ({
-//     time = nowInSeconds() + 45,
-//     address = DEFAULT_CONTRACT,
-//   }) => {
-//     const [ owner ] = await ethers.getSigners()
+task('setSaleStart', `Sets the start of the sale`)
+  .addOptionalParam('address', `The contract address`)
+  .addOptionalParam('time', `The timestamp in seconds since Unix Epoch`)
+  .setAction(async ({
+    time = nowInSeconds() + 45,
+    address = DEFAULT_CONTRACT,
+  }) => {
+    const [ owner ] = await ethers.getSigners()
 
-//     const provider = new ethers.providers.JsonRpcProvider()
-//     const punkscape = new ethers.Contract(address, abi, provider)
+    const provider = new ethers.providers.JsonRpcProvider()
+    const punkscape = new ethers.Contract(address, abi, provider)
 
-//     // Set start time to past
-//     console.log('Setting start time to ', time)
-//     await punkscape.connect(owner).setSaleStart(time)
-//   })
+    // Set start time to past
+    console.log('Setting start time to ', time)
+    await punkscape.connect(owner).setSaleStart(time)
+  })
